64539467dc86cdb66a86c2e50678c90080947234
CMDB-127710 <n.popov@itransition.com>
2022-11-15 09:41:34 +0100

    initial commit

---
+++ SwiftUICoreDataCourse/SwiftUICoreDataCourseApp.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUICoreDataCourseApp.swift
+//  SwiftUICoreDataCourse
+//
+//  Created by CMDB-127710 on 14.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICoreDataCourseApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
